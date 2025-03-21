51e481463e003a259ef003e85d45633453b9e9b2
1bobbywen@gmail.com <1bobbywen@gmail.com>
2025-03-21 12:35:09 -0700

    update loadbalancer file

--- terraform-aws-ecs-frgt-base-infra/load_balancer.tf
+++ terraform-aws-ecs-frgt-base-infra/load_balancer.tf
@@ -26,15 +26,15 @@ resource "aws_lb_target_group" "ecs-fargate-TG" {
   protocol    = "HTTP"
   vpc_id      = aws_vpc.main.id
   target_type = "ip"
-  health_check {
-    healthy_threshold   = "3"
-    interval            = "90"
-    protocol            = "HTTP"
-    matcher             = "200-299"
-    timeout             = "20"
-    path                = "/"
-    unhealthy_threshold = "2"
-  }
+  # health_check {
+  #   healthy_threshold   = "3"
+  #   interval            = "90"
+  #   protocol            = "HTTP"
+  #   matcher             = "200-299"
+  #   timeout             = "20"
+  #   path                = "/"
+  #   unhealthy_threshold = "2"
+  # }
   tags = {
     Application_LoadBalancer = "aws_lb.cluster_lb.name"
   }
